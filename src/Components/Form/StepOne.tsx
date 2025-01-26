efd8fd0eab6551a21cbe377409526a76e93acf6f
Davesakraken <davidgeorgebell1995@outlook.com>
2025-01-26 22:56:23 +0000

    create initial form context

--- src/Components/Form/StepOne.tsx
+++ src/Components/Form/StepOne.tsx
@@ -1,29 +1,31 @@
+import { useFormContext } from "@/src/Context/FormContext.";
+import { useContext } from "react";
+
 export default function StepOne() {
+  const { formData, setFormData } = useFormContext();
   return (
     <>
-      <form>
-        <label htmlFor="fname">Name</label>
-        <input
-          className="w-full h-10 outline outline-1 rounded-lg p-2 outline-lightGray mt-2"
-          type="text"
-          id="fname"
-          name="fname"
-        ></input>
-        <label htmlFor="femail">Email Address</label>
-        <input
-          className=" w-full h-10 outline outline-1 rounded-lg p-2 outline-lightGray mt-2"
-          type="email"
-          id="femail"
-          name="fmail"
-        ></input>
-        <label htmlFor="fnumber">Phone Number</label>
-        <input
-          className="w-full h-10 outline outline-1 rounded-lg p-2 outline-lightGray mt-2"
-          type="text"
-          id="fnumber"
-          name="fnumber"
-        ></input>
-      </form>
+      <label htmlFor="fname">Name</label>
+      <input
+        className="w-full h-10 outline outline-1 rounded-lg p-2 outline-lightGray mt-2"
+        type="text"
+        id="fname"
+        name="fname"
+      ></input>
+      <label htmlFor="femail">Email Address</label>
+      <input
+        className=" w-full h-10 outline outline-1 rounded-lg p-2 outline-lightGray mt-2"
+        type="email"
+        id="femail"
+        name="fmail"
+      ></input>
+      <label htmlFor="fnumber">Phone Number</label>
+      <input
+        className="w-full h-10 outline outline-1 rounded-lg p-2 outline-lightGray mt-2"
+        type="text"
+        id="fnumber"
+        name="fnumber"
+      ></input>
     </>
   );
 }
---
+++ src/Context/FormContext..tsx
@@ -0,0 +1,39 @@
+"use client";
+import { createContext, ReactNode, useContext, useState } from "react";
+import invariant from "tiny-invariant";
+
+interface FormData {
+  name: string;
+  emailAddress: string;
+  phoneNumber: number;
+  plan: string;
+  billingCycle: "monthly" | "yearly";
+  addons: string[];
+}
+
+interface FormContextType {
+  formData: FormData;
+  setFormData: React.Dispatch<React.SetStateAction<FormData>>;
+}
+
+const FormContext = createContext<FormContextType | null>(null);
+
+export const useFormContext = () => {
+  const ctx = useContext(FormContext);
+  invariant(!ctx, "Context called outside of provider");
+  return ctx;
+};
+
+const defaultFormValues: FormData = {
+  name: "",
+  emailAddress: "",
+  phoneNumber: 0,
+  plan: "",
+  billingCycle: "monthly",
+  addons: [],
+};
+
+export const FormProvider = ({ children }: { children: ReactNode }) => {
+  const [formData, setFormData] = useState<FormData>(defaultFormValues);
+  return <FormContext.Provider value={{ formData, setFormData }}>{children}</FormContext.Provider>;
+};
