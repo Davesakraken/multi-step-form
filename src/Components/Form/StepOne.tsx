002c730b989ad40bdfc848b6d5a0757df489aef8
Davesakraken <davidgeorgebell1995@outlook.com>
2025-02-05 23:19:37 +0000

    personal info field margin

--- src/Components/Form/StepOne.tsx
+++ src/Components/Form/StepOne.tsx
@@ -6,30 +6,36 @@ export default function StepOne() {
 
   return (
     <>
-      <label htmlFor="fname">Name</label>
-      <input
-        className="w-full h-10 outline outline-1 rounded-lg p-2 outline-lightGray mt-2"
-        type="text"
-        id="fname"
-        name="fname"
-        onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-      ></input>
-      <label htmlFor="femail">Email Address</label>
-      <input
-        className=" w-full h-10 outline outline-1 rounded-lg p-2 outline-lightGray mt-2"
-        type="email"
-        id="femail"
-        name="fmail"
-        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-      ></input>
-      <label htmlFor="fnumber">Phone Number</label>
-      <input
-        className="w-full h-10 outline outline-1 rounded-lg p-2 outline-lightGray mt-2"
-        type="string"
-        id="fnumber"
-        name="fnumber"
-        onChange={(e) => setFormData({ ...formData, tel: e.target.value })}
-      ></input>
+      <div>
+        <label htmlFor="fname">Name</label>
+        <input
+          className="w-full h-10 outline outline-1 rounded-lg p-2 outline-lightGray mt-2"
+          type="text"
+          id="fname"
+          name="fname"
+          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+        ></input>
+      </div>
+      <div className="mt-4">
+        <label htmlFor="femail">Email Address</label>
+        <input
+          className=" w-full h-10 outline outline-1 rounded-lg p-2 outline-lightGray mt-2"
+          type="email"
+          id="femail"
+          name="fmail"
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        ></input>
+      </div>
+      <div className="mt-4">
+        <label htmlFor="fnumber">Phone Number</label>
+        <input
+          className="w-full h-10 outline outline-1 rounded-lg p-2 outline-lightGray mt-2"
+          type="string"
+          id="fnumber"
+          name="fnumber"
+          onChange={(e) => setFormData({ ...formData, tel: e.target.value })}
+        ></input>
+      </div>
     </>
   );
 }
