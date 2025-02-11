27c2cd799299414cbe2defa589116215f8c328d7
Davesakraken <davidgeorgebell1995@outlook.com>
2025-02-11 19:45:55 +0000

    context validation

--- src/Context/FormContext..tsx
+++ src/Context/FormContext..tsx
@@ -22,6 +22,9 @@ interface FormContextType {
   setFormData: React.Dispatch<React.SetStateAction<FormData>>;
   summaryData: SummaryData;
   setSummaryData: React.Dispatch<React.SetStateAction<SummaryData>>;
+  errors: {};
+  handleFormSubmit: () => void;
+  validateForm: (stepNumber: number) => boolean;
 }
 
 const FormContext = createContext<FormContextType | null>(null);
@@ -46,22 +49,66 @@ const defaultSummaryValues: SummaryData = {
   billingPeriodSuffix: "/mo",
 };
 
-const formSchema = z.object({
-  name: z.string(),
-  email: z.string(),
-  tel: z.string(),
-  plan: z.string(),
-  billingPeriod: z.union([z.literal("monthly"), z.literal("yearly")]),
-});
-
-// create errors object that is returned with any errors
-
 export const FormProvider = ({ children }: { children: ReactNode }) => {
   const [formData, setFormData] = useState<FormData>(defaultFormValues);
   const [summaryData, setSummaryData] = useState<SummaryData>(defaultSummaryValues);
-  console.table(formData);
+  const [errors, setErrors] = useState<Record<string, string>>({});
+
+  const pageOneSchema = z.object({
+    name: z.string().min(1, "Name is required"),
+    email: z.string().email("Invalid email format"),
+    tel: z.string().min(10, "Phone number must be at least 10 digits"),
+  });
+
+  const pageTwoSchema = z.object({
+    tel: z.string().min(10, "Phone number must be at least 10 digits"),
+  });
+
+  const validateForm = (pageNumber: number) => {
+    let result;
+    if (pageNumber === 1) {
+      result = pageOneSchema.safeParse(formData);
+    } else if (pageNumber === 2) {
+      result = pageTwoSchema.safeParse(formData);
+    } else {
+      return;
+    }
+
+    if (!result.success) {
+      const formattedErrors: Record<string, string> = {};
+
+      result.error.errors.forEach((err) => {
+        if (err.path.length) {
+          formattedErrors[err.path[0]] = err.message;
+        }
+      });
+      setErrors(formattedErrors);
+      return false;
+    }
+    setErrors({});
+    return true;
+  };
+
+  const handleFormSubmit = (pageNumber: number) => {
+    if (validateForm(pageNumber)) {
+      console.log("form submitted successfully!");
+    } else {
+      console.log("validation errors", errors);
+    }
+  };
+
   return (
-    <FormContext.Provider value={{ formData, setFormData, summaryData, setSummaryData }}>
+    <FormContext.Provider
+      value={{
+        formData,
+        setFormData,
+        summaryData,
+        setSummaryData,
+        errors,
+        handleFormSubmit,
+        validateForm,
+      }}
+    >
       {children}
     </FormContext.Provider>
   );
