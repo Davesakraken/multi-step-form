489ceffdf58262fcd1d1affdf1aba6b1bfe0b217
Davesakraken <davidgeorgebell1995@outlook.com>
2025-02-17 22:30:04 +0000

    refactor to MultiStepForm

--- src/Components/common/Container.tsx
+++ src/Components/common/Container.tsx
@@ -1,9 +1,14 @@
-import { PropsWithChildren } from "react";
+import StepList from "@/src/Components/stepList/StepList";
+import Form from "@/src/Components/Form/Form";
+import { useState } from "react";
 
-export default function Container({ children }: PropsWithChildren) {
+export default function MultiStepForm() {
+  const [step, setStep] = useState(1);
+
   return (
     <section className="flex h-[100vh] w-full md:h-[35rem] md:w-[53rem] bg-white md:rounded-xl p-5 justify-between">
-      {children}
+      <StepList stepNumber={step} />
+      <Form stepNumber={step} setStepNumber={setStep} />
     </section>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
 "use client";
 
-import Container from "@/src/Components/common/Container";
-import Form from "@/src/Components/Form/Form";
-import StepList from "@/src/Components/stepList/StepList";
-import { useState } from "react";
+import MultiStepForm from "@/src/Components/common/Container";
 
 export default function Home() {
-  const [step, setStep] = useState(1);
-
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <Container>
-        <StepList stepNumber={step} />
-        <Form stepNumber={step} setStepNumber={setStep} />
-      </Container>
+      <MultiStepForm />
     </main>
   );
 }
