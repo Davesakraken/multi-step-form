db8b9a9702693140b94b2f066ff79a5289548b32
Davesakraken <davidgeorgebell1995@outlook.com>
2025-03-10 21:56:00 +0000

    add fixed container for StepList

--- src/Components/stepList/StepList.tsx
+++ src/Components/stepList/StepList.tsx
@@ -10,26 +10,26 @@ interface StepListProps {
 export default function StepList({ stepNumber }: StepListProps) {
   const listItems = ["YOUR INFO", "SELECT PLAN", "ADD-ONS", "SUMMARY"];
   const desktopClasses =
-    "md:justify-start md:rounded-lg md:flex-col md:bg-sidebar-desktop md:p-8 md:gap-0";
+    "md:rounded-lg md:p-8 md:bg-sidebar-desktop md:items-start md:justify-start md:min-w-[15.5rem] md:h-full";
 
   return (
-    <ol
-      className={`flex flex-row justify-center gap-5 min-w-[15.5rem] md:h-[100%] bg-sidebar-mobile text-white bg-cover py-16 ${desktopClasses}`}
-    >
-      {listItems.map((item, idx) => (
-        <li
-          key={idx}
-          className={clsx(
-            stepNumber === idx + 1 && `${styles.stepItemHighlight}`,
-            `${styles.stepListItem} flex gap-4 md:pb-5`
-          )}
-        >
-          <div className="hidden md:flex md:flex-col">
-            <div className="font-extralight text-xs tracking-tight">STEP {idx + 1}</div>
-            <div className="font-bold text-sm tracking-wider">{item}</div>
-          </div>
-        </li>
-      ))}
-    </ol>
+    <section className={`flex justify-center bg-sidebar-mobile bg-cover h-44 ${desktopClasses}`}>
+      <ol className="flex flex-row md:flex-col text-white gap-6 md:gap-0 mb-12">
+        {listItems.map((item, idx) => (
+          <li
+            key={idx}
+            className={clsx(
+              stepNumber === idx + 1 && `${styles.stepItemHighlight}`,
+              `${styles.stepListItem} flex gap-4 md:pb-5`
+            )}
+          >
+            <div className="hidden md:flex md:flex-col">
+              <div className="font-extralight text-xs tracking-tight">STEP {idx + 1}</div>
+              <div className="font-bold text-sm tracking-wider">{item}</div>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </section>
   );
 }
